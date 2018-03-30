c8cc6ea30ad1c01638ecfa77bab95e65da85eb5a
blasco991 <blasco991@gmail.com>
2018-03-30 23:08:09 +0200

    Init commit

--- session1/tree_search.py
+++ session1/tree_search.py
@@ -2,29 +2,93 @@ import gym
 import gym_ai_lab
 import search.algorithms as search
 
+envs = ["SmallMaze-v0", "GrdMaze-v0", "BlockedMaze-v0"]
 
-envname = "SmallMaze-v0"
+for envname in envs:
+    print("\n----------------------------------------------------------------")
+    print("\tTREE SEARCH")
+    print("\tEnvironment: ", envname)
+    print("----------------------------------------------------------------\n")
 
-print("\n----------------------------------------------------------------")
-print("\tTREE SEARCH")
-print("\tEnvironment: ", envname)
-print("----------------------------------------------------------------\n")
+    env = gym.make(envname)
+    env.render()
 
-env = gym.make(envname)
-env.render()
+    # DFS
+    solution, _, stats = search.dls_ts(env, -1)
+    print("\n\nDFS:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
 
-# Suggestion: test the depth-limited search by itself before IDS
+    # IDS
+    solution, stats = search.ids(env, search.dls_ts)
+    if solution is not None:
+        solution = [env.state_to_pos(s) for s in solution]
+    print("\n\nIDS:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
 
-# IDS
-solution, stats = search.ids(env, search.dls_ts)
-if solution is not None:
-    solution = [env.state_to_pos(s) for s in solution]
-print("\n\nIDS:\n----------------------------------------------------------------"
-      "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}".format(
-        round(stats[0], 4), stats[1], stats[2], solution))
+    # BFS
+    solution, stats = search.bfs(env, search.tree_search_plus)
+    if solution is not None:
+        solution = [env.state_to_pos(s) for s in solution]
+    print("\n\nBFS TS+:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
 
-# BFS
-solution, stats = search.bfs(env, search.tree_search)
-# .. print ...
+    # UCS
+    solution, stats = search.ucs(env, search.tree_search_plus)
+    if solution is not None:
+        solution = [env.state_to_pos(s) for s in solution]
+    print("\n\nUCS TS+:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
 
-# And so forth...
+    # A*
+    solution, stats = search.astar(env, search.tree_search_plus)
+    if solution is not None:
+        solution = [env.state_to_pos(s) for s in solution]
+    print("\n\nA* TS+:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
+
+    # Greedy
+    solution, stats = search.greedy(env, search.tree_search_plus)
+    if solution is not None:
+        solution = [env.state_to_pos(s) for s in solution]
+    print("\n\nGreedy TS+:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
+
+    # BFS
+    solution, stats = search.bfs(env, search.tree_search)
+    if solution is not None:
+        solution = [env.state_to_pos(s) for s in solution]
+    print("\n\nBFS:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
+
+    # UCS
+    solution, stats = search.ucs(env, search.tree_search)
+    if solution is not None:
+        solution = [env.state_to_pos(s) for s in solution]
+    print("\n\nUCS:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
+
+    # A*
+    solution, stats = search.astar(env, search.tree_search)
+    if solution is not None:
+        solution = [env.state_to_pos(s) for s in solution]
+    print("\n\nA*:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
+
+    """
+    # Greedy
+    solution, stats = search.greedy(env, search.tree_search)
+    if solution is not None:
+        solution = [env.state_to_pos(s) for s in solution]
+    print("\n\nGreedy:\n----------------------------------------------------------------"
+          "\nExecution time: {0}s\nN° of states expanded: {1}\nMax n° of states in memory: {2}\nSolution: {3}"
+          .format(round(stats[0], 4), stats[1], stats[2], solution))
+    """
