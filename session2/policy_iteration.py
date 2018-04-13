801df97cd686cf2285c76ba08387e7f076bd941a
Riccardo <riccardo.sartea@univr.it>
2018-04-13 11:56:06 +0200

    - Parameters tuning

--- session2/policy_iteration.py
+++ session2/policy_iteration.py
@@ -9,9 +9,9 @@ from timeit import default_timer as timer
 delta = 1e-3
 gamma = 1.0
 pmaxiters = 50  # Max number of policy improvements to perform
-vmaxiters = 5  # Max number of iterations to perform while evaluating a policy
+vmaxiters = 10  # Max number of iterations to perform while evaluating a policy
 
-envname = "NiceLavaFloor-v0"
+envname = "LavaFloor-v0"
 
 print("\n----------------------------------------------------------------")
 print("\tEnvironment: ", envname)
