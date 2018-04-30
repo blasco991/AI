67dab9dc0de11fe0f7540ff1cf45fb05a4c7408c
riccardo <riccardo.sartea@univr.it>
2018-04-30 11:28:34 +0200

    - Fixes

--- session2/examplecode.py
+++ session2/examplecode.py
@@ -22,6 +22,20 @@ print("T[0, 1, 1]: ", env.T[0, 1, 1])
 # Reward R(0, 'R', 1): reward for transitioning from state (0, 0) to (0, 1) with action 'R'
 print("R[0, 1, 1]: ", env.R[0, 1, 1])
 
+# Transition probabilities from state 10. Result is indexed by action/state: row -> action, column -> destination state
+print("Transition probabilities from state 10:\n", env.T[10])
+
+# Reward that can be achieved from state 10 (without considering transition probability).
+# Result is indexed by state.
+print("Possible rewards from state 10 with action R:\n", env.R[10, 1])
+
+# Best reward from state 10 (1-step lookahead)
+pr = env.T[10] * env.R[10]
+print("Best possible reward from state 10: ", pr.max())
+
+# Best action to perform from state 10 (1-step lookahead)
+print("Best acton from state 10: ", env.actions[pr.max(axis=1).argmax()])
+
 # Verify that T is a probability, i.e., for each pair of state/action the sum is 1 (on the third axis of the 3d-array)
 print("Sum of probability values:\n", env.T.sum(axis=2))  # Power of numpy
 
